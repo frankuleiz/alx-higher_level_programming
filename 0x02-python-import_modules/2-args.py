bcda250b33a5548170aef2018f41dc6e47eab73a
Frank <kamunyuz@gmail.com>
2023-08-11 14:50:11 -0700

    arguements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
 argc = len(argv)
 if argc < 2:
     print("{} arguements.".format(argc - 1))
-elif argc == 2:
-    print("{} arguement:".format(argc - 1))
+else:
+    if argc == 2:
+        print("{} arguement:".format(argc - 1))
     else:
         print("{} arguements:".format(argc - 1))
     for i in range(1, argc):
