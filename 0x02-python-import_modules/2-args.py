b7a95b60b4fb77e80e8ec02877922def164d55d0
Frank <kamunyuz@gmail.com>
2023-08-11 15:02:16 -0700

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-argc = len(argv)
-if argc < 2:
-    print("{} arguements.".format(argc - 1))
-else:
-    if argc == 2:
+    argc = len(argv)
+    if argc < 2:
+        print("{} arguements.".format(argc - 1))
+    elif argc == 2:
         print("{} arguement:".format(argc - 1))
     else:
         print("{} arguements:".format(argc - 1))
