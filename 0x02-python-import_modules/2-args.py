e7aaae53e9b1acb19a3a19172b59429082cc5d17
Frank <kamunyuz@gmail.com>
2023-08-11 14:35:47 -0700

    arguements

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+argc = len(argv)
+if argc < 2:
+    print("{} arguements.".format(argc - 1))
+elif argc == 2:
+    print("{} arguement:".format(argc - 1))
+else:
+    print("{} arguements:".format(argc -1))
+for i in range(1, argc):
+    print("{}: {}".format(i, argv[i]))
