c0682e7098350eb038f9d2fac2435a6c8d352f23
Frank <kamunyuz@gmail.com>
2023-08-11 16:21:31 -0700

    hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    name =dir(hidden_4)
+    name = dir(hidden_4)
     length = len(name)
     for i in range(0, length):
         if name[i][0:2] != "__":
