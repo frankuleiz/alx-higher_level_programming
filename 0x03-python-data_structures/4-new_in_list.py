8027e7afc4f2732761649db3165579b5a3dea3e5
Frank <kamunyuz@gmail.com>
2023-08-13 13:13:48 -0700

    replaces an index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    copy_list = my_list.copy()
+    copy_list = my_list.copy)
+    length = len(my_list)
     if idx < 0:
         return (copy_list)
-    if idx >= (len(my_list) - 1):
+    if idx >= length - 1:
         return (copy_list)
     else:
         copy_list[idx] = element
