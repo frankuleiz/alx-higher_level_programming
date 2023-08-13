206417834957354b65fd1e4e453eb184ba341513
Frank <kamunyuz@gmail.com>
2023-08-13 13:05:25 -0700

    replaces an index

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    copy_list = my_list.copy()
+    if idx < 0:
+        return (copy_list)
+    if idx >= len(my_list - 1):
+        return (copy_list)
+    else:
+        copy_list[idx] = element
+        return (copy_list)
+
