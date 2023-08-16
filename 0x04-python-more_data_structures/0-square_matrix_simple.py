2998403cac1280e9a80a30637ad305a1c359bda3
Frank <kamunyuz@gmail.com>
2023-08-16 08:30:28 -0700

    square root

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(len(matrix)):
-        new_matrix[row] = list(map(lambda x: x ** 2, new_matrix[row]))
-    return new_matrix
+    if not matrix:
+        return None
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
