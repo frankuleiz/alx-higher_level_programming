abc5ccedec99c4cbbc5076105c5917c52be78184
Frank <kamunyuz@gmail.com>
2023-08-16 18:06:01 -0700

    corrections

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     """
-    function that computes the square value of all integers of a matrix using map
+    function computes the square value of all integers of a matrix using map
     matrix is a 2 dimensional array
     Returns a new matrix
     """
