b85e6a7dc23ad1a67119c089098977adc8f1c1f2
Frank <kamunyuz@gmail.com>
2023-08-16 17:25:59 -0700

    replaces a key

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """
+    a function that replaces or adds key/value in a dictionary
+    a function that replaces or adds key/value in a dictionary.
+    value argument will be any type
+    If a key exists in the dictionary, the value will be replaced
+    If a key doesn’t exist in the dictionary, it will be created
+    """
     a_dictionary[key] = value
     return a_dictionary
