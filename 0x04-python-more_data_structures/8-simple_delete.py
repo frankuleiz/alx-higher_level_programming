40b4de9ad0f9ee0abbae596ed53065cff7b444e7
Frank <kamunyuz@gmail.com>
2023-08-16 17:29:50 -0700

    commenting

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    """
+    function that deletes a key in a dictionary
+    key argument will be always a string
+    If a key doesn’t exist, the dictionary won’t change
+    """
     if not a_dictionary:
         return a_dictionary
     if key in a_dictionary:
