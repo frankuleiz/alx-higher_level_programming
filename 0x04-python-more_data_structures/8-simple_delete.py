277939629059265f2b4c82d8b53a4ab270374b37
Frank <kamunyuz@gmail.com>
2023-08-16 17:32:21 -0700

    commenting

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -9,4 +9,4 @@ def simple_delete(a_dictionary, key=""):
         return a_dictionary
     if key in a_dictionary:
         del a_dictionary[key]
-        return a_dictionary
+    return a_dictionary
