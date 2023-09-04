e0fcbb796aee27b09060b0a77157e407361f5364
Frank <kamunyuz@gmail.com>
2023-09-04 03:23:49 +0300

    print users names

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+
+    
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/pythom3
+"""
+The '3-say_my_name' module.
+This module passes one function say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    A function that prints first and last name
+
+    Args:
+        first_name: the users's first name of type string
+        last_name: the user's last ane of type string
+
+    Return: user's full name
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
