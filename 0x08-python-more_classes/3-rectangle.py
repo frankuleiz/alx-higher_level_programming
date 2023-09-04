26c63c796e7c4a80430611bc6cb41b8b7f68e3b4
Frank <kamunyuz@gmail.com>
2023-09-05 00:37:45 +0300

    printable string

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -92,12 +92,15 @@ class Rectangle:
         else:
             return ((self.__width * 2) + (self.__height * 2))
 
-        def __str__(self):
-            """
-            A method that returns a printable string representation of the
-            rectangle
-            """
-            string = ""
-            if self.__width != 0 and self.__height != 0:
-                string += "\n".join("#" * self.__width for j in range(self.__height))
-            return string
+    def __str__(self):
+        """
+        A method that returns string representation of rectangle
+        """
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                string += "#" * self.__width
+                if i + 1 != self.__height:
+                    string += "\n"
+
+        return string
