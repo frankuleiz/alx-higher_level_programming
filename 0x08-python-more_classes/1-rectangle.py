cf0afc62f64977b313edb3059bef249f38313596
Frank <kamunyuz@gmail.com>
2023-09-04 21:42:08 +0300

    with attributes corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,54 +1,78 @@
 #!/usr/bin/python3
-"""Defines a class of Rectangle"""
+"""
+Module 1-rectangle
+Defines a rectangle class
+"""
 
 
 class Rectangle:
+    """
+    Defines class Rectangle
+
+    Methods:
+        __init__(): creates an object instance
+        width(): width attribute
+        height(): height atttribute
+    """
 
     def __init__(self, width=0, height=0):
         """
-    A class rectangle definition.
-    The rectangle with private attributes width and  height
-    """
+        A method that initializes a Rectangle instance
+
+        Args:
+            width: the width size of the rectangle
+            height: the height size of the rectangle
+        """
+
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        """Getter method to retrieve the width of the rectangle"""
+        """
+        Getter method to retrieve width of the rectangle
+
+        Return:
+            width attribute
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        The setter method of __width
+        Setter method to get the value of the width
+
         Args:
-            value: gets the value of height
-        Return: None
+            Value: an int value of the width
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """The getter method to retrieve the height of the rectangle"""
+        """
+        Getter method  to get the value of the height
+
+        Return:
+            height attribute
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        The setter method of __height
-        Args:
-            value: the value of height
-        Return: None
-        """
+        The getter method to update the value of height
 
+        Args:
+            value: an int value of the height
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
