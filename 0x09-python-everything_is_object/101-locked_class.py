c2abb2f0f048102ca79b27ab36dc654f18f52068
Frank <kamunyuz@gmail.com>
2023-09-06 00:41:40 +0300

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The 101-locked_class module"""
+
+
+class LockedClass:
+    """
+    A class LockedClass with an attribute to be set dynamically __slots__
+    """
+    __slots__ = ['first name']
