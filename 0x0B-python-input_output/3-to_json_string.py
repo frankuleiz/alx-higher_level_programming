553f727338fa0fd628ffc25a63d8403740a50f83
Frank <kamunyuz@gmail.com>
2023-09-12 22:34:15 +0300

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The ``3-to_json_string``"""
+
+
+def to_json_string(my_obj):
+    """
+    Function to change a string to a json representation
+    """
+    return json.dumps(obj)
