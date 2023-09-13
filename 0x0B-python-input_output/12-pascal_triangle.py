3c98903af65d406b580dcfbd43dfa5dd7b97adcf
Frank <kamunyuz@gmail.com>
2023-09-13 05:42:36 +0300

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""The ``12-pascal_triangle``module"""
+
 
 def pascal_triangle(n):
+    """A function to print a pascal triangle"""
     if n <= 0:
         return []
 
@@ -9,9 +12,8 @@ def pascal_triangle(n):
         row = [1]
         if x > 1:
             for i in range(1, len(pascal[x - 1])):
-                row.append(pascal[x -1][i - 1] + pascal[x - 1][i])
+                row.append(pascal[x - 1][i - 1] + pascal[x - 1][i])
         row.append(1)
         pascal.append(row)
 
     return pascal
-
