f9a560ba56b8e6d3f4bf897f285ca31872de6e58
Frank <kamunyuz@gmail.com>
2023-09-20 12:15:35 +0300

    correction for str

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,7 +29,7 @@ class Square(Rectangle):
         """
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
 
         self.width = value
