17cf4561a039cda206120ffa6c543221e9f1eeeb
Frank <kamunyuz@gmail.com>
2023-10-17 21:16:04 +0300

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- A ipt that creates a table second_table in the database hbtn_0c_0
---If the table second_table already exists, your script should not fail
+-- If the table second_table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
