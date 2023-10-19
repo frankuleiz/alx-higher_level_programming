157e56a588e1ad677d2cbc3cf3c3679a500d3ff0
Frank <kamunyuz@gmail.com>
2023-10-19 05:22:02 +0300

    cities by state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,8 @@
 -- A ript that lists all cities contained in the database hbtn_0d_usa.
-    -- Each record should display: cities.id - cities.name - states.name
-    -- Results must be sorted in ascending order by cities.id
-    -- You can use only one SELECT statement
-    -- The database name will be passed as an argument of the mysql command
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
 
 SELECT cities.id, cities.name, states.name
 From cities
