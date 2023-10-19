dc749b67b456ea7c9a30719a976aedde3f344284
Frank <kamunyuz@gmail.com>
2023-10-19 05:08:26 +0300

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -14,5 +14,5 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY
     states_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(states_id) REFERENCES(hbtn_0d_usa.states(id)),
+    FOREIGN KEY(states_id) REFERENCES(hbtn_0d_usa.states(id))
 );
