828cb1603cee4a8b9a706ec9f48cc8bfb8903203
Frank <kamunyuz@gmail.com>
2023-10-19 05:11:08 +0300

    cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,7 +12,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa ;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY
-    states_id INT NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY(states_id) REFERENCES(hbtn_0d_usa.states(id))
 );
