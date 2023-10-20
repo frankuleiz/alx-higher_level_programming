09b77077355f12e57a28b12aac492cf1ec13ae3d
Frank <kamunyuz@gmail.com>
2023-10-20 05:18:35 +0300

    corrections

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,6 +4,5 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states AS s
-WHERE cities.state_id = s.id
-ORDER BY cities.id ASC;
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
