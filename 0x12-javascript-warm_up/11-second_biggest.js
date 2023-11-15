cfb7eca3f15c6b119e5143ef0a9863155af415a5
Frank <kamunyuz@gmail.com>
2023-11-15 23:58:55 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
+// Get all the arguments except the script name
 const args = process.argv.slice(2);
 
 if (args.length <= 1) {
   console.log(0);
 } else {
+  // Convert the arguments to integers and sort them in descending order
   const num = args.map(Number).sort((a, b) => b - a);
-
+  // Locate the second biggest integer (at index 1 after sorting )
   console.log(num[1]);
 }
