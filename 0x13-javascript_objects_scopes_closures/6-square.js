e9fefbd75249de9784a01b9bb40b95317d8bb2dd
Frank <kamunyuz@gmail.com>
2023-11-15 15:17:46 +0300

    Square 1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Base = require('./5-square');
+
+class Square extends Base {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
