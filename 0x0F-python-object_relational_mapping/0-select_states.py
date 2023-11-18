9e5d83cbedeb711a3dfe1077dee731335d2a7615
Frank <kamunyuz@gmail.com>
2023-11-18 14:35:18 +0300

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ = "__main__":
+    """
+    The access to get states from the database
+    """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port="3306",
                          passwd=argv[2], db=argv[3])
