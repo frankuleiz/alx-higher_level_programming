d855c26078185f3752e9db2257d1f55a51d10ea0
Frank <kamunyuz@gmail.com>
2023-11-19 15:36:38 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
 """
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
+The script takes in the name of a state as an argument amd lists
+all cities of that state in the database that is SQL injection free
 """
-
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the cities
-    from the database.
-    """
+state_name = argv[4]
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+db = MySQLdb.connect(host="localhost", username=argv[1],
+                     passwd=argv[2], port="3306", db=argv[3])
 
-    with db.cursor() as cur:
-        cur.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
+cur = db.cursor()
+sql_query = "SELECT cities.id, cities.name \
+             FROM cities \
+             JOIN states \
+             ON cities.state_id = states.id \
+             WHERE states.name LIKE BINARY %s \
+             ORDER BY cities.id ASC"
+cur.execute(sql_query, (state_name,))
+rows = cur.fetchall()
 
-        rows = cur.fetchall()
-
-    if rows is not None:
-        print(", ".join([row[1] for row in rows]))
+if rows is not None:
+    print(", ".join([row[1] for row in rows]))
