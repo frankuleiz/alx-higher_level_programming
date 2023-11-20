39bfd47b5355df1d851d5d1f25e05e2290eacc54
Frank <kamunyuz@gmail.com>
2023-11-20 19:07:55 +0300

    sql

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+The script that prints the forst state object from the database
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -6,6 +9,9 @@ from sqlalchemy.orm import Session, sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    The access to the database and get a state from from the database
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True))
     Session = sessionmaker(bind=engine)
