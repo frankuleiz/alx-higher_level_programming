d6ea62780bdb0379cb9f7e73dc82f98aa03626e0
Frank <kamunyuz@gmail.com>
2023-11-21 16:25:08 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/pythonon3
 """
 The script that prints the state object with the name passed
 as arguement from the database
@@ -9,6 +9,9 @@ from sqlalchemy.orm import sessionmaker, Session
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    Gives access to the database to get the state object
+    """
     db = 'mysql+_mysqldb//:{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(db)
